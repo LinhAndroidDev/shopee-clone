dde75b58f73d072cf4d065d7330e9429a29b4567
zyyx-linhnh <linhnh@zyyx.jp>
2025-06-30 10:53:39 +0700

    add file local keys

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,26 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const common_home = 'common.home';
+  static const common_mall = 'common.mall';
+  static const common_live_video = 'common.live_video';
+  static const common_notification = 'common.notification';
+  static const common_personal = 'common.personal';
+  static const common = 'common';
+  static const home_shopee_mall = 'home.shopee_mall';
+  static const home_shopee_pay_wallet = 'home.shopee_pay_wallet';
+  static const home_googleplay_link_easy_payment = 'home.googleplay_link_easy_payment';
+  static const home_roll_call = 'home.roll_call';
+  static const home_to_get_coins = 'home.to_get_coins';
+  static const home_spay_later = 'home.spay_later';
+  static const home_activate_to_receive = 'home.activate_to_receive';
+  static const home_shopee_live_super_cheap = 'home.shopee_live_super_cheap';
+  static const home_super_sale = 'home.super_sale';
+  static const home_live = 'home.live';
+  static const home_flash_sale = 'home.flash_sale';
+  static const home_hot_selling = 'home.hot_selling';
+  static const home = 'home';
+
+}
